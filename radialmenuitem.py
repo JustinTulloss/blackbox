a5064532ec46fc7c41234626c5496ca74bba2d09
jmtulloss <none@none>
2007-04-20 10:56:42 -0700

    [svn r36] Commented RadItem better
    --HG--
    branch : pylin

--- radialmenuitem.py
+++ radialmenuitem.py
@@ -3,7 +3,7 @@
 #
 #Justin Tulloss
 #
-#
+#Trying to comment well for Steve
 
 import gtk
 from gtk import gdk
@@ -12,11 +12,14 @@ import math
 
 class RadItem(gtk.DrawingArea):
 
+    #Background colors for various events
     _normalBG = (0,44/255,1)
     _downBG = (2,102/255,0)
     _overBG = (165/255,1,0)
 
+    #Constructor
     def __init__(self, text=""):
+        #Calls gtk.DrawingArea's constructor
         super(RadItem, self).__init__()
 
         #hook up all necessary events
@@ -31,7 +34,8 @@ class RadItem(gtk.DrawingArea):
                         gdk.BUTTON_RELEASE_MASK |
                         gdk.ENTER_NOTIFY_MASK |
                         gdk.LEAVE_NOTIFY_MASK)
-        self._text = text
+
+        self._text = text #What this button will say
 
         self._activeBG = self._normalBG
 
@@ -82,12 +86,12 @@ class RadItem(gtk.DrawingArea):
         self._centerY = rect.y + rect.height/2
 
         #set up a transform
-        #make sure we're always in the middle
-        #sets us up to have a 1.0x1.0 surface
+        #TODO:make sure we're always in the middle
+            #How do I do this?
+        #sets us up to have a 1.0x1.0 square surface scaled to the actual size
         context.scale(min(rect.width,rect.height),min(rect.width,rect.height))
-        #context.scale(rect.width,rect.height)
 
-        self._radius = .5
+        self._radius = .5 #half the size of the widget
 
         #draw the circle and background
         self.draw_circle(context)
@@ -100,37 +104,49 @@ class RadItem(gtk.DrawingArea):
         cr.set_line_width(.01)
         bgcolor = self._activeBG
 
-        bgGrad = cairo.LinearGradient(.5, 0, .5, 1)
-        bgGrad.add_color_stop_rgba(1, 1, 1, 1, .5)
+        #Create background gradient from the center top to the center bottom
+        bgGrad = cairo.LinearGradient(x0=.5, y0=0, x1=.5, x2=1)
+        bgGrad.add_color_stop_rgba(offset=1, red=1, green=1, blue=1, alpha=.5)
         bgGrad.add_color_stop_rgba(0, bgcolor[0] , bgcolor[1], bgcolor[2], .8)
 
+        #actually draw the circle (center x, center y, radius, degrees)
         cr.arc(.5, .5, self._radius, 0, 2*math.pi)
         cr.set_source(bgGrad)
-        cr.fill_preserve()
+        cr.fill_preserve() #fill in the circle with the gradient
 
+        #Draw outline around the circle (probably won't keep this)
         cr.set_source_rgba(0, 0, 0, 1)
         cr.stroke()
 
     def draw_text(self, cr):
         cr.set_source_rgba(0, 0, 0, .5) #font color
+
+        #This finds how much space the text currently takes up. 
         x, y, width, height = cr.text_extents(self._text)[:4]
 
         #TODO: Make font size dynamic
-        cr.set_font_size(.2) #Font size
+        cr.set_font_size(.2) #Font size, should be resized from above
+
+        #recalculate how much space the text takes with new font size
         x, y, width, height = cr.text_extents(self._text)[:4]
+        #go to the top left of the above rectangle if we were to center it
         cr.move_to(.5-width/2-x, .5-height/2-y)
-        cr.show_text(self._text)
+        cr.show_text(self._text) #actually draw the text from where we are now
 
     def draw_effect(self, cr):
+
+        #Gradient from transparent to white
         grad = cairo.LinearGradient(.5, 0, .5, 1)
         grad.add_color_stop_rgba(0, 1, 1, 1, .85)
         grad.add_color_stop_rgba(1, 1, 1, 1, 0)
 
+        #What we're doing here is drawing the same circle, but scaling it.
+        # It should look like a glassy glow on the top
         cr.save() #save off old context
-        cr.translate(.5-.33,.01)
-        cr.scale(.66, .4)
-        cr.arc(.5, .5, self._radius, 0, 2*math.pi)
-        cr.set_source(grad)
+        cr.translate(.5-.33,.01) #move smaller circle to the middle
+        cr.scale(.66, .4) #squish the drawing area
+        cr.arc(.5, .5, self._radius, 0, 2*math.pi) #draw in squishe area
+        cr.set_source(grad) #set up gradient to be filled in
         cr.fill()
         cr.restore() #restore old context
 
